225d06c9a86d2b10753bb6b6794660014be7b685
Subhajit Das <jitsubha7980@gmail.com>
2025-07-11 05:37:24 +0000

    Variables and datatype done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,28 @@
+const accountId = 144553        // const means it cannot be changed 
+let accountEmail = "jitsubha7980@gmail.com"
+var accountPassword ="12345"
+accountCity = "Jaipur"     // this is not a good way
+let accountState          // undefined 
+
+
+// accountId = 2 // not allowed
+
+accountEmail = "abcs@gmail.com"
+
+accountPassword = "12"
+
+accountCity = "kolkata"
+
+console.log(accountId); // to print
+
+/*
+prefer not to use var 
+because of issue in block scope and functional scope
+*/
+
+console.log([accountEmail, accountPassword, accountCity]);   // easy method for multible variables for print
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]) // this gives it in a tabular structure
+
+
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,39 @@
+"use strick"; // treat all codes as newer version
+
+// alert(4+4)  // we are using nodejs, not browser
+
+console.log(3+3);
+console.log("Subhajit");
+
+
+let Name = "Subhajit"
+let Age = 19
+let IsLoggedIn = false
+let State;
+let Religion = null
+
+
+// number => 2 to the power of 53
+// bigint =>
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof Name);
+
+console.log(typeof State);
+
+console.log(typeof Age);
+
+console.log(typeof IsLoggedIn);
+
+console.log(typeof Religion);  // "Note": type of null is object
+
+
+
+
