2a6f8da722ddbc5f20e66b745d176f09f46cb761
Subhajit Das <jitsubha7980@gmail.com>
2025-07-12 03:23:28 +0000

    coversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score = "subhajit"
+
+console.log(typeof score);
+console.log(typeof (score)); // method 2
+
+let valueInNumber = Number(score)
+console.log(typeof (valueInNumber));
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33Abc" => NaN = not a number 
+// true => 1, false => 0
+
+let isLoggedIN ="Subha"
+
+let booleanIsLoggedIn = Boolean(isLoggedIN)
+
+console.log(booleanIsLoggedIn);
+
+
+// 1 => true, 0 => false
+// "" => false, "subha" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
