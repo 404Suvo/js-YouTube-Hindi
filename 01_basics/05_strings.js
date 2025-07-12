fd43c64ad101ac0a3608e2cc9c9ee052e2e4e40d
Subhajit Das <jitsubha7980@gmail.com>
2025-07-12 17:10:11 +0000

    strings done

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,37 @@
+const name = "Subhajit"
+const repoCount = 50
+
+console.log(name + repoCount + " Value");    // this is not a good way
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Subhajit Das-is-a-great-coder-and-a-man')
+
+ console.log(gameName[0]);   // access thhe elements
+ console.log(gameName.__proto__);    // tells the prototype
+
+
+console.log(gameName.length);   // tells the length of the string
+console.log(gameName.toUpperCase());  // converts all in uppercase
+console.log(gameName.charAt(4));     // access the element by giving the index
+console.log(gameName.indexOf('a'));  // tells the index ofthe element by giving the element
+
+const newString = gameName.substring(0, 4)   // Note this rule: len(string) - value
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)    // it show only the sliced part
+console.log(anotherString);
+
+const newStringOne = "   hitesh    "
+console.log(newStringOne);
+console.log(newStringOne.trim());   // removes all spaces from start and end
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20', '-'))   // repalce the value
+
+console.log(url.includes('sundar'))    // finding the element is there or not
+
+console.log(gameName.split('-'));
+console.log(gameName.split('-', 5)); // it splits, where the given element is there and we can also set a limit upto how much to split
+
