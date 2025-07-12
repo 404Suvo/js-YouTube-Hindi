9ab7f6b2dfdbd1d666535fab0f64d35725ca96a8
Subhajit Das <jitsubha7980@gmail.com>
2025-07-12 19:37:38 +0000

    array

---
+++ 01_basics/02_basics/01_arrays.js
@@ -0,0 +1,7 @@
+// Javascript arrays copies operations creates shallow copies which share the same reference and deep copies which do not share the same reference 
+
+const myArr = [0,1,2,3,4,5,6,7,8,9]    // collection of datatypes
+const myHeros = ["Naruto", "Itachi"]
+
+const myArr2 = new Array(1,2,3,4)
+console.log(myArr[0]);
