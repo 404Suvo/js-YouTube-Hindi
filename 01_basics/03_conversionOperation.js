b895fd928480e965eecee2d35729527459897545
Subhajit Das <jitsubha7980@gmail.com>
2025-07-12 04:00:03 +0000

    operators complete

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -29,6 +29,58 @@ let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
 
+// ******************************* Operation *********************************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2/2);
+console.log(2**2);
+console.log(2%2);
+console.log(2/2);
+console.log(2*2);
+
+let str1 = "hello"
+let str2 = "Subhajit"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+
+console.log(((3 + 4)* 5)% 3);  // always use paranthesis
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3 
+
+num1 = num2 = num3 = 2 + 2  // not a good way
+
+let gameCounter = 100
+++gameCounter;           // prefix operator
+gameCounter++;           // postfix operator
+console.log(gameCounter);
+
+// link to study
+//https://262.ecma-international.org/#sec-type-conversion
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
