2a1cf703fa1448b920c8a778ee876e1ec6c9f1fc
Subhajit Das <jitsubha7980@gmail.com>
2025-07-12 19:21:17 +0000

    date complete

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,52 @@
+// Dates
+
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.toTimeString());
+console.log(myDate.toUTCString());
+console.log(myDate.getTimezoneOffset());
+console.log(typeof myDate);   // it is a object
+
+let myCreatedDate = new Date(2021, 0, 22, 9, 3, 2)  // year, month, date, hour, minute, second => month starts from 0
+console.log(myCreatedDate.toLocaleString());
+
+let myCreatedDate2 = new Date("2005-12-29")  // we can also give our own format but we mostly prefer DD/MM/YYYY
+console.log(myCreatedDate2.toLocaleString());
+
+let myTimestamp = Date.now()   // milisecond value
+
+console.log(myTimestamp);
+console.log(myCreatedDate.getTime());
+console.log(Math.floor(Date.now()/1000));
+
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);   // for month it is month + 1 and also many func are there for date year....
+
+newDate.toLocaleString('default',{
+    weekday: "long"
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
