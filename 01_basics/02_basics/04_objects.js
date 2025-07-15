e9f67e00817af8e374cf3500e5f05b9b00b853bc
Subhajit Das <jitsubha7980@gmail.com>
2025-07-15 17:43:38 +0000

    objects part 2

--- 01_basics/02_basics/04_objects.js
+++ 01_basics/02_basics/04_objects.js
@@ -1,11 +1,11 @@
 // objects singleton
-/*
+
 const tinderUser = new Object()    // this is a singleton object
 
-const tinderUser.id = "123abc"
+tinderUser.id = "123abc"
 tinderUser.name = "Samay"
 tinderUser.isLoggedIn = false;
-
+/*
 console.log(tinderUser);
 
 const regularUser = {
@@ -23,9 +23,53 @@ console.log(regularUser.fullname?.userfullname["firstname"]);    // access the n
 
 const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "c", 4: "d"}
-const obj3 ={obj1, obj2}
+/*
+console.log(obj1, obj2);        // method 1
+console.log(...obj1, ...obj2);   // method 2 */
 
-console.log(obj1, obj2);
-console.log();
+const obj3 = Object.assign({}, obj1, obj2)  // method 3 and the empty space is good to put because it acts as a target and the other as a source
+console.log(obj3);
+
+
+const users = [
+    {
+        id: 1,
+        email: "subha@gmail.com"
+
+    },
+    {
+        id: 2,
+        email: "rohan@gmail.com"
+
+    },
+    {
+        id: 3,
+        email: "rimi@gmail.com"
+
+    },
+    {
+        id: 4,
+        email: "priti@gmail.com"
+
+    },
+    {
+        id: 5,
+        email: "avi@gmail.com"
+
+    }
+]
+
+console.log(users[1].email);
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));     // keys into arrays
+console.log(Object.values(tinderUser));    // values into arrays 
+console.log(Object.entries(tinderUser));  // every keys and values forms into arrays
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));   // checks if the property is available or not
+
+
+
+
 
 
