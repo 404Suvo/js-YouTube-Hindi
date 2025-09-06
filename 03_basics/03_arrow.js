1a01f459323536b45fc003a2557bdffef98084ce
Subhajit Das <jitsubha7980@gmail.com>
2025-09-06 06:15:05 +0000

    This Due

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -1,27 +1,27 @@
-const user = {
-    username: "subhajit",
-    price: 999,
+// const user = {
+//     username: "subhajit",
+//     price: 999,
 
-    welcomeMessage: function(){
-        console.log(`${this.username}, welcome to website`);    // this makes current context means all the variables in the scope can access through this
-        // console.log(this);
+//     welcomeMessage: function(){
+//         console.log(`${this.username}, welcome to website`);    // this makes current context means all the variables in the scope can access through this
+//         // console.log(this);
         
-    }
+//     }
 
-}
+// }
 
 //  user.welcomeMessage()
 //  user.username = "sam"
 //  user.welcomeMessage()
 
-console.log(this);  // it will give empty
+// console.log(this);  // it will give empty
 
-// function chai(){
+// function chai1(){
 //     let username = "subha"
-//     console.log(this.username);  // undefined in functions
+//     console.log(this);  // many data is there in thr object
     
 // }
-// chai()
+// chai1()
 
 // const chai = function(){
 //     let username = "Subhajit"
@@ -32,13 +32,30 @@ console.log(this);  // it will give empty
 // chai()
 
 
- const chai = () => {       // arrow function
-     let username = "Subhajit"
-     console.log(this.username);  // arrow function in variable also this is undefined
+//  const chai2 = () => {       // arrow function
+//      let username = "Subhajit"
+//      console.log(this);  // gives an empty object
     
+//  }
+
+//  chai2()
+
+const obj3 = {
+    Name: "subhajit",
+    Age: 20,
+    Course: "CSE",
+    chai3: function(){
+        this.Name((Output) => {
+        console.log(this.Name, + Output);
+        
+    })
  }
 
-//  chai()
+}
+
+obj3.chai3()
+
+
 
 
 // Arrow function
@@ -53,14 +70,14 @@ console.log(this);  // it will give empty
 
 // Implicit return
 
-const addTwo = (num1, num2) =>  (num1 + num2)   // implicit means i believe you don't need to write return
-                                                // explicit return is when return is applied
-console.log(addTwo(5,9))
+// const addTwo = (num1, num2) =>  (num1 + num2)   // implicit means i believe you don't need to write return
+//                                                 // explicit return is when return is applied
+// console.log(addTwo(5,9))
 
 
-const addThree = (num1, num2) =>  ({username: "Subhajit"})   // returning object
+// const addThree = (num1, num2) =>  ({username: "Subhajit"})   // returning object
 
-console.log(addThree(5,9))
+// console.log(addThree(5,9))
 
 
 // const myArray = [1,2,4,5,6]
